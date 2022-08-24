ff8aefd13c57c3dcb92404d5443cedf73a28e36a
Dyeuse <dyeuse@gmail.com>
2022-08-24 15:41:26 -0400

    Fix Eslint format

--- src/components/form/dataForm.jsx
+++ src/components/form/dataForm.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Measurements from "./measurements/measurements";
-import Options from "./options/options";
-import "./dataForm.css";
+import React from 'react';
+import Measurements from './measurements/measurements';
+import Options from './options/options';
+import './dataForm.css';
 
 function DataForm() {
-  return (
-    <form>
-      <div className="personal-data">
-        <Measurements />
-        <Options />
-      </div>
-      <button type="submit">Calculer</button>
-    </form>
-  );
+    return (
+        <form>
+            <div className="personal-data">
+                <Measurements />
+                <Options />
+            </div>
+            <button type="submit">Calculer</button>
+        </form>
+    );
 }
 
 export default DataForm;
--- src/components/form/measurements/measurements.jsx
+++ src/components/form/measurements/measurements.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Measurement from "./measurement";
-import "./measurements.css";
+import React from 'react';
+import Measurement from './measurement';
+import './measurements.css';
 
 function Measurements() {
-  return (
-    <div className="measurements">
-      <Measurement label="height" unity="cm" />
-      <Measurement label="weight" unity="kg" />
-      <Measurement label="waist" unity="cm" />
-      <Measurement label="neck" unity="cm" />
-      <Measurement label="hip" unity="cm" />
-    </div>
-  );
+    return (
+        <div className="measurements">
+            <Measurement label="height" unity="cm" />
+            <Measurement label="weight" unity="kg" />
+            <Measurement label="waist" unity="cm" />
+            <Measurement label="neck" unity="cm" />
+            <Measurement label="hip" unity="cm" />
+        </div>
+    );
 }
 
 export default Measurements;
