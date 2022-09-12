c9ee0b95c082dd05f7f68f98c421ee2879d1c11b
Dyeuse <dyeuse@gmail.com>
2022-09-12 10:24:19 -0400

    Update cards titles

--- src/components/results/cards.jsx
+++ src/components/results/cards.jsx
@@ -3,15 +3,15 @@ import Card from "./card";
 import "./cards.css";
 
 function Cards() {
-  return (
-    <div className="cards">
-      <Card title="IMC" />
-      <Card title="IMG" />
-      <Card title="FFMI" />
-      <Card title="BM" />
-      <Card title="DEE" />
-    </div>
-  );
+    return (
+        <div className="cards">
+            <Card title="BMI" />
+            <Card title="BFP" />
+            <Card title="FFMI" />
+            <Card title="BMR" />
+            <Card title="TDEE" />
+        </div>
+    );
 }
 
 export default Cards;
