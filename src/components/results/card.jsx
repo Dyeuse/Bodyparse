82f38be652a457786bb05e75e8c72bd19ff4d3a5
Dyeuse <dyeuse@gmail.com>
2022-09-19 14:34:49 -0400

    Connect calculs with cards

--- src/components/results/card.jsx
+++ src/components/results/card.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Card({ title }) {
-  return (
-    <div>
-      <h2>{title}</h2>
-      <div>---</div>
-    </div>
-  );
+function Card({ title, result }) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <div>{result}</div>
+        </div>
+    );
 }
 
 Card.propTypes = {
-  title: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    result: PropTypes.number.isRequired,
 };
 
 export default Card;
--- src/components/results/cards.jsx
+++ src/components/results/cards.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Card from "./card";
+import { calcBMI, calcBFP, calcFFMI, calcBMR, calcTDEE } from "../calculs/formulas";
 import "./cards.css";
 
-function Cards() {
+function Cards({ personalData }) {
+    const BMI = calcBMI(personalData);
+    const BFP = calcBFP(personalData);
+    const FFMI = calcFFMI(personalData, BFP);
+    const BMR = calcBMR(personalData, BFP);
+    const TDEE = calcTDEE(personalData, BMR);
     return (
         <div className="cards">
-            <Card title="BMI" />
-            <Card title="BFP" />
-            <Card title="FFMI" />
-            <Card title="BMR" />
-            <Card title="TDEE" />
+            <Card title="BMI" result={BMI} />
+            <Card title="BFP" result={BFP} />
+            <Card title="FFMI" result={FFMI} />
+            <Card title="BMR" result={BMR} />
+            <Card title="TDEE" result={TDEE} />
         </div>
     );
 }
