a3e24199a8b6a07885fb2a7619034f34a31fdeb7
Dyeuse <dyeuse@gmail.com>
2022-10-20 11:02:00 -0400

    Add card design

--- src/components/results/card.jsx
+++ src/components/results/card.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import "./card.css";
 
 function Card({ children, result }) {
+    const [isHidden, setisHidden] = useState(true);
+    const onClickHandler = useCallback((e) => {
+        if ((e.type === "keydown" && e.keyCode === 13) || e.type === "click") {
+            setisHidden((d) => !d);
+        }
+    });
+
     return (
         <div className="card">
-            <h2>{children}</h2>
-            <div className="result">{result}</div>
+            <input
+                type="checkbox"
+                className="toggleDefinition"
+                onClick={onClickHandler}
+                onKeyDown={onClickHandler}
+            />
+            <div className="arrow">
+                {isHidden ? <span>&#129134;</span> : <span>&#129132;</span>}
+            </div>
+            <div
+                className={`cardCountent concept ${
+                    isHidden ? "displayed" : "undisplayed"
+                }`}
+            >
+                <h2>{children}</h2>
+                <div>{result}</div>
+            </div>
+            <div
+                className={`cardCountent definition ${
+                    isHidden ? "undisplayed" : "displayed"
+                }`}
+            >
+                <h3>Definition</h3>
+                <div>Blablabla</div>
+            </div>
         </div>
     );
 }
